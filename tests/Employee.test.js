e2edfebb1c5be2ea9b0181a621e246dfc68fccb4
Tim Scott <0timothy.scott0@gmail.com>
2021-02-25 18:50:55 -0500

    employee class & tests

---
+++ tests/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe("getId", () => {
+        it("should return correct id", () => {
+            const id = 1;
+            const e = new Employee(id, "", "");
+            expect(e.getId()).toEqual(id);
+        });
+    });
+    describe("getName", () => {
+        it("should return correct name", () => {
+            const name = "jason";
+            const e = new Employee(0, name, "");
+            expect(e.getName()).toEqual(name);
+        });
+    });
+    describe("getEmail", () => {
+        it("should return correct email", () => {
+            const email = "jason";
+            const e = new Employee(0, "", email);
+            expect(e.getEmail()).toEqual(email);
+        });
+    });
+    describe("getRole", () => {
+        it("should return correct role", () => {
+            const e = new Employee(0, "", "");
+            expect(e.getRole()).toEqual("Employee");
+        });
+    });
+});
